a65dc44c3e9a9653d7c8da0988dc60549fd302b2
Dimos <dimosthenis.manias@gmail.com>
2022-07-11 12:15:09 +0300

    remove shops unused

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,7 +2,6 @@ package graph
 
 import (
 	"github.com/go-redis/redis/v8"
-	"github.com/pampatzoglou/api/graph/model"
 )
 
 // This file will not be regenerated automatically.
@@ -10,6 +9,6 @@ import (
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	shops []*model.Shop
+	// shops []*model.Shop
 	Redis redis.Client
 }
